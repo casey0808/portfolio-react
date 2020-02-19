a7b8632b618396789c43668c19a0f7bd97164bb6
casey0808 <casey_c0808@outlook.com>
2020-02-19 12:21:09 +0800

    update

--- src/Project.js
+++ src/Project.js
@@ -9,14 +9,14 @@ class Project extends Component {
         return (
             <div>
                 <div className="project-tile">
-                    <Tile src={Images[0]} text="Color Game" link="http://casey0808.github.io/web_development/color_game/colorGame.html" />
-                    <Tile src={Images[1]} text="Todo List App" link="https://casey0808.github.io/todo-list/" />
-                    <Tile src={Images[2]} text="Random Quote Machine" link="https://elated-wing-438675.netlify.com/" />
-                    <Tile src={Images[3]} text="Interactive Graph of AQI" link="http://casey0808.github.io/aqi_shanghai/pm25_sh.html" />
-                    <Tile src={Images[4]} text="Data Visualization with D3" link="https://casey0808.github.io/fcc-data-visualization/" />
-                    <Tile src={Images[5]} text="Geospatial Data Visualization" link="https://github.com/casey0808/real_estate_study" />
-                    <Tile src={Images[6]} text="Patatap Clone" link="http://casey0808.github.io/web_development/patatap_clone/circles.html" />
-                    <Tile src={Images[7]} text="Data Science Projects" link="https://github.com/casey0808/datacamp_projects" />
+                    <Tile src={Images[0]} text="Color Game" link="http://casey0808.github.io/web_development/color_game/colorGame.html" github="https://github.com/casey0808/web_development" />
+                    <Tile src={Images[1]} text="Todo List App" link="https://casey0808.github.io/todo-list/" github="https://github.com/casey0808/todo-list" />
+                    <Tile src={Images[2]} text="Random Quote Machine" link="https://elated-wing-438675.netlify.com/" github="https://github.com/casey0808/fcc-random-quote-machine" />
+                    <Tile src={Images[3]} text="Interactive Graph of AQI" link="http://casey0808.github.io/aqi_shanghai/pm25_sh.html" github="https://github.com/casey0808/aqi_shanghai" />
+                    <Tile src={Images[4]} text="Data Visualization with D3" link="https://casey0808.github.io/fcc-data-visualization/" github="https://github.com/casey0808/fcc-data-visualization" />
+                    <Tile src={Images[5]} text="Geospatial Data Visualization" link="https://cancui.work/real_estate_study/" github="https://github.com/casey0808/real_estate_study" />
+                    <Tile src={Images[6]} text="Patatap Clone" link="http://casey0808.github.io/web_development/patatap_clone/circles.html" github="https://github.com/casey0808/web_development" />
+                    <Tile src={Images[7]} text="Data Science Projects" link="https://cancui.work/datacamp_projects/" github="https://github.com/casey0808/datacamp_projects" />
                 </div>
                 <a className="button" href="https://casey0808.github.io/project.html" target="_blank" rel="noopener noreferrer">See More >></a>
             </div>
--- src/Tile.js
+++ src/Tile.js
@@ -7,7 +7,11 @@ class Tile extends Component {
             <div className="Tile">
                 <img className="Tile-img" src={this.props.src} alt={this.props.text}></img>
                 <p>{this.props.text}</p>
-                <a href={this.props.link} target="_blank" rel="noopener noreferrer"><i className="fa fa-link"> play online</i></a>
+                <span>
+                    <a href={this.props.link} target="_blank" rel="noopener noreferrer"><i className="fa fa-link"> play online </i></a>
+                    <span> | </span>
+                    <a href={this.props.github} target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+                </span>
             </div>
         );
     }
