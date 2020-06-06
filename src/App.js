e73f4d9c3d7b93b63f33bd7d234934e4a6ff300d
casey0808 <casey_c0808@outlook.com>
2020-06-06 12:59:18 +0800

    update projects

--- src/App.js
+++ src/App.js
@@ -6,15 +6,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <nav className="navbar">
-        <h2 className="header">Casey C</h2>
-        <NavLink exact to="/" activeClassName="active-nav" className="nav-text">Project</NavLink>
-        <NavLink exact to="/about" activeClassName="active-nav" className="nav-text">About</NavLink>        
+    <div className='App'>
+      <nav className='navbar'>
+        <h2 className='header'>Casey C</h2>
+        <NavLink exact to='/' activeClassName='active-nav' className='nav-text'>
+          Project
+        </NavLink>
+        <NavLink
+          exact
+          to='/about'
+          activeClassName='active-nav'
+          className='nav-text'
+        >
+          About
+        </NavLink>
       </nav>
-      <Switch className="main">
-        <Route exact path="/" render={() => <Project />} />
-        <Route exact path="/About" render={() => <About />} />
+      <Switch className='main'>
+        <Route exact path='/' render={() => <Project />} />
+        <Route exact path='/About' render={() => <About />} />
       </Switch>
     </div>
   );
--- src/Images.js
+++ src/Images.js
@@ -1,4 +1,4 @@
-import aqi_shanghai from './img/aqi_shanghai.png';
+// import aqi_shanghai from './img/aqi_shanghai.png';
 import colorgame from './img/colorgame.png';
 import ds from './img/ds.png';
 import heatmap from './img/heatmap.png';
@@ -8,16 +8,17 @@ import markercluster from './img/markercluster.png';
 import todo_list from './img/todo_list.png';
 import wes from './img/wes.png';
 import markdown from './img/markdown_previewer.png';
+import pomo_clock from './img/pomo_clock.png';
 
 const Images = [
-    colorgame,
-    todo_list,
-    markdown,
-    aqi_shanghai,
-    heatmap,
-    markercluster,
-    wes,
-    ds,
+  colorgame,
+  todo_list,
+  markdown,
+  pomo_clock,
+  heatmap,
+  markercluster,
+  wes,
+  ds,
 ];
 
 export default Images;
